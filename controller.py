2f5cde8ec63854132037adb3ffc44c9a0b2e96f6
chominhu <minhu0409@gmail.com>
2024-11-23 21:59:33 +0900

    1123

--- controller.py
+++ controller.py
@@ -3,7 +3,7 @@ API_KEY = "RGAPI-6fdc0d77-ae7a-466b-b001-41e653eff791"
 TOKEN_URL = 'https://kauth.kakao.com/oauth/token'
 CSECRET ="BGDkauFdjU9lEtb1n6G7tesgpoNNONwb"
 CID = "57f9d0c11d0039471ba6a9d38162c466"
-RURI= "http://127.0.0.1:5000/callback"   #REDIRECT_URI  #https://127.0.0.1:5000/callback
+RURI= "https://minhu.site/callback"   #REDIRECT_URI  #https://127.0.0.1:5000/callback
 KAKAO_LOGIN_URL = "https://kauth.kakao.com/oauth/authorize?client_id="+CID+"&redirect_uri="+RURI+"&scope=profile_nickname,profile_image,talk_message&response_type=code"
 
 def getMatchHistroy(gameName, tagLine,region,country):
